6ce249d2c184925c404668a2d92931f1877a8388
Dokaaa <122877634+Dokaaa@users.noreply.github.com>
2023-02-07 22:20:21 +0600

    Delete 4 directory

--- 4/4/d.py
+++
@@ -1,6 +0,0 @@
-sum = 0
-n = 5
-for i in range(1, n + 1):
-    sum += i
-print(sum)
-# (a,b) начинает с а, заканчивает с b-1
--- 4/4/zadachki/8.py
+++
@@ -1,7 +0,0 @@
-n=int(input())
-sum=1
-total=0
-for i in range (1,n+1):
-    sum*=i
-    total+=sum
-print(total)
